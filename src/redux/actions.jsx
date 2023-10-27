fa451e45b9d09566ba938de4d5d83c1fbb8e2e06
MartinVillegas25 <martinvillegas90@hotmail.com>
2023-10-27 10:13:27 -0300

    https

--- src/redux/actions.jsx
+++ src/redux/actions.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-const url = 'http://menu-host-back-production.up.railway.app'
+const url = 'https://menu-host-back-production.up.railway.app'
 import axios from 'axios';
 
 export const CREATE_USER = 'CREATE_USER';
