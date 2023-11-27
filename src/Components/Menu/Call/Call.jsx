2560dc82f3d6ab6a42753e8bf3351e95c927296f
MartinVillegas25 <martinvillegas90@hotmail.com>
2023-11-27 10:38:21 -0300

    socket

--- src/Components/Menu/Call/Call.jsx
+++ src/Components/Menu/Call/Call.jsx
@@ -2,7 +2,7 @@ import { useLocation } from 'react-router-dom';
 import io from 'socket.io-client';
 import './Call.css';
 import { PiCallBellDuotone } from 'react-icons/pi';
-const socket = io();
+const socket = io("https://simesero.com");
 //const socket = io('https://menu-didactico.up.railway.app/');
 
 export default function Call() {
@@ -27,6 +27,7 @@ export default function Call() {
 	};
 	// Funcion para enviar la alerta de llamar camarera
 	const handleSubmit = () => {
+		console.log('anda el boton');
 		socket.emit('llamar-camarera', usuario, payload);
 	};
 
